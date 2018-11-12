d896156b43e4c795d0309f00d3c1634a0fe8c65f
Ammar <ammar.alnatsheh@gmail.com>
2018-11-12 10:57:36 -0800

    finish Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  this.prototype = Object.create(Grub.prototype);
+  this.age = 5;
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.job = 'keep on growing';
+
 };
 
+Bee.prototype.job = function() {
+  return this.job;
+};
