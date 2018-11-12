c004921b4aa30b066b35fafa5c6f9f8b5e909e4c
Ammar <ammar.alnatsheh@gmail.com>
2018-11-12 11:16:11 -0800

    finish RetiredforeagerBee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
+  this.prototype = Object.create(ForagerBee.prototype);
+
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function() {
+  this.treasureChest.push('treasure');
+};
