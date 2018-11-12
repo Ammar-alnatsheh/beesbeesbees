1e38e824c86357bd694167b5519744c6999947fd
Ammar <ammar.alnatsheh@gmail.com>
2018-11-12 10:58:00 -0800

    finish HoneyMakerBee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
 var HoneyMakerBee = function() {
+  this.prototype = Object.create(Bee.prototype);
+
+  this.age = 10;
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+
+Bee.prototype.makeHoney = function() {
+  this.honeyPot ++;
+};
+
+Bee.prototype.giveHoney = function() {
+  this.honeyPot --;
+};
